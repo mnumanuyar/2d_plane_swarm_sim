aeb69e6a0a73b5dcce59450269e1bc7e8d1b0700
numan <m.numanuar@gmail.com>
2024-05-10 22:11:17 +0300

    add spawner

---
+++ scripts/spawner.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var plane = preload("res://scenes/plane.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for x in range(-10,10,1):
+		for y in range(-10,10,1):
+			var instance =plane.instantiate()
+			instance.global_transform.origin.x = x*30
+			instance.global_transform.origin.y = y*30
+			add_child(instance)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
